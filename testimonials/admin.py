bb23f7efea9ea681b5f59b3c38a558508ccbfe51
bpotterschipul <bpotter@schipul.com>
2013-01-21 11:26:53 -0600

    Adding the ability to attach a photo to add-on.
    Signed-off-by: bpotterschipul <bpotter@schipul.com>

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.utils.text import truncate_words
 from django.utils.html import strip_tags
+from django.core.urlresolvers import reverse
 
 from tendenci.core.perms.admin import TendenciBaseModelAdmin
 from addons.testimonials.models import Testimonial
 from addons.testimonials.forms import TestimonialForm
+from tendenci.core.event_logs.models import EventLog
+from tendenci.core.perms.utils import update_perms_and_save
 
 
 class TestimonialAdmin(TendenciBaseModelAdmin):
-    list_display = ['first_last_name', 'testimonial_parsed', 'create_dt']
+    list_display = ['image_preview', 'first_last_name', 'testimonial_parsed', 'create_dt']
     list_filter = ['last_name', 'first_name', 'create_dt']
     search_fields = ['first_name', 'last_name', 'testimonial']
     ordering = ('-create_dt',)
@@ -16,6 +19,7 @@ class TestimonialAdmin(TendenciBaseModelAdmin):
         (None, {'fields': (
             'first_name',
             'last_name',
+            'photo_upload',
             'testimonial',
             'tags',
         )}),
@@ -50,5 +54,36 @@ class TestimonialAdmin(TendenciBaseModelAdmin):
         testimonial = truncate_words(testimonial, 50)
         return testimonial
     testimonial_parsed.short_description = 'testimonial'
+
+    def save_model(self, request, object, form, change):
+        testimonial = form.save(commit=False)
+
+        # save photo
+        if 'photo_upload' in form.cleaned_data:
+            photo = form.cleaned_data['photo_upload']
+            if photo:
+                testimonial.save(photo=photo)
+
+        testimonial = update_perms_and_save(request, form, testimonial)
+
+        log_defaults = {
+            'instance': object,
+            'action': "edit"
+        }
+        if not change:
+            log_defaults['action'] = "add"
+
+        return object
+
+    def image_preview(self, obj):
+        if obj.image:
+            args = [obj.image.pk]
+            args.append("100x50")
+            args.append("crop")
+            return '<img src="%s" />' % reverse('file', args=args)
+        else:
+            return "No image"
+    image_preview.allow_tags = True
+    image_preview.short_description = 'Image'
 
 admin.site.register(Testimonial, TestimonialAdmin)
