51e1cdbbb6732f19a281760b33c8f2e618f20f79
Jenny Qian <jqian@tendenci.com>
2015-05-08 10:02:49 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+longdesc = \
+'''
+A Tendenci testimonials app.
+'''
+
+setup(
+    name='tendenci-testimonials',
+    author='Tendenci',
+    author_email='programmers@tendenci.com',
+    version='1.0.0',
+    license='GPL3',
+    description='Testimonials app for Tendenci',
+    long_description=longdesc,
+    url='https://github.com/tendenci/tendenci-testimonials',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=['tendenci>=6.0'],
+)
